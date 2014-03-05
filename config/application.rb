19cc9fd15436cddef944cc7ac408d6d2a4e0f833
Nicholas Linsky <nick.linsky@gmail.com>
2014-03-05 01:50:37 -0500

    Make a basic User model (including secure passwords)

--- config/application.rb
+++ config/application.rb
@@ -11,14 +11,12 @@ require "sprockets/railtie"
 # you've limited to :test, :development, or :production.
 Bundler.require(:default, Rails.env)
 
-config.secret_key_base = 'blipblapblup'
-
 module SampleApp
   class Application < Rails::Application
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-
+    I18n.enforce_available_locales = true
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
